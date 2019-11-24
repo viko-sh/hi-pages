e7c0d9e03a360191882522d65f35d744fd28df95
Viktor <viktor.wrk@gmail.com>
2019-11-24 13:16:36 +1100

    Add cors module  to accept requests from the client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,25 @@
 import * as express from 'express';
 import jobRoutes from './routes/api/jobsRoutes';
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const server = express();
 // const port = 8080;
 const port = 9000;
-
+server.use(cors());
+server.use(bodyParser.json());
 server.use('/api/jobs', jobRoutes);
 
-server.use(function(error, req, res, next) {
-  // Will **not** get called. You'll get Express' default error
-  // handler, which returns `error.toString()` in the error body
-  console.log('will not print');
-  res.json({ message: error.message });
-});
+// server.use('*', (req, res, next) => {
+//   console.log('HERE');
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   // res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization'
+//   );
+//   next();
+// });
 
 server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
